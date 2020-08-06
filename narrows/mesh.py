Fix edge off-by-one error
If we have N fence segments (cells_per_region) then we require N+1 fence
posts (edges), so use num=N+1 instead of num=N in np.linspace() call.
Specifically, consider the case of N=50. Our arrays are then

     edge nn_flux sn_flux mc_flux an_flux
size 51     51      51      50      51

sn_flux is 51 because our sn implementation solves for flux at each edge
nn_flux is 51 in order to mimic sn_flux
an_flux is 51 in order to mimic sn_flux
mc_flux is 50 because our mc implementation solves for flux in each cell

narrows/mesh.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def create_mesh(deck):
-    edges = [np.linspace(x.start, x.end, deck.ctrl.cells_per_region,
+    edges = [np.linspace(x.start, x.end, deck.ctrl.cells_per_region + 1,
                          dtype=np.float32) for x in deck.reg.values()]
     edges = np.unique(np.concatenate(edges))
     mats = [None] * len(edges)