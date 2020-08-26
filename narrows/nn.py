a2b4da50e16094c9eaf554e2a577523693c13f02
Mike Pozulp <pozulp1@llnl.gov>
2020-08-26 16:28:14 -0700

    Cleanup nn implementation
    Remove use_weights. Kyle added this boolean in a commit with message
    "Added weight regularization to ANN" from November 2018. Remove this
    feature as we do not seem to be using it.
    
    Omit _t variable name suffix. I think Kyle used _t to denote the
    torch.Tensor variable type.
    
    Use single quotes ' instead of double quotes ".
    
    Remove less-than-useful comments, including the file docstring.
    
    Add LARGE_DOUBLE global constant to replace magic number.

--- narrows/nn.py
+++ narrows/nn.py
@@ -1,25 +1,22 @@
-"""
-Class for solving the 1D neutron transport equation in a slab geometry.
-Author: Kyle Bilton
-Date: 11/2018
-"""
 import numpy as np
 import torch
 
 from .writer import write
 
+LARGE_DOUBLE = 1e50
+
 
 class ANNSlabSolver(object):
-    """
+    '''
     Solver for the slab geometry neutron transport equation using neural
     networks. The ANN uses a single hidden layer.
-    """
+    '''
 
     def __init__(self, N, n_nodes, edges, sigma_t, sigma_s0, sigma_s1, source,
                  gamma_l=50, gamma_r=50, learning_rate=1e-3, eps=1e-8,
-                 use_weights=False, tensorboard=False, interval=500,
-                 gpu=False, ahistory=False, hinterval=1):
-        """
+                 tensorboard=False, interval=500, gpu=False, ahistory=False,
+                 hinterval=1):
+        '''
         Parameters
         ==========
         N : int
@@ -44,8 +41,6 @@ class ANNSlabSolver(object):
             Learning rate of the Adam optimizer.
         eps : float
             Convergence criterion comparison quantity.
-        use_weights : bool
-            Use updated residual weights in minimizing the loss.
         tensorboard : bool
             Use tensorboard.
         interval : int
@@ -56,7 +51,7 @@ class ANNSlabSolver(object):
             Record loss and flux arrays every hinterval iterations
         hinterval : int
             The number of interations between recordings
-        """
+        '''
 
         self.device = torch.device('cpu')
         self.gpu = False
@@ -66,6 +61,7 @@ class ANNSlabSolver(object):
                 self.gpu = True
             else:
                 write('terse', 'Skipping gpu')
+
         ########################################
         # Angular Meshing
         ########################################
@@ -74,25 +70,20 @@ class ANNSlabSolver(object):
 
         # Get the Legendre-Gauss quadratures
         mu, w = np.polynomial.legendre.leggauss(N)
-        self.mu = mu
-        self.w = w
+
         # Put the quadratures into tensors
-        mu_t = torch.tensor(mu.astype(np.float32), device=self.device)
-        w_t = torch.tensor(w.astype(np.float32), device=self.device)
-        self.mu_t = mu_t
-        self.w_t = w_t
+        self.mu = torch.tensor(mu.astype(np.float32), device=self.device)
+        self.w = torch.tensor(w.astype(np.float32), device=self.device)
 
         # Turn the edges into a PyTorch tensor, stacked as a column
-        # (hence the [:,None])
-        self.z = torch.from_numpy(edges[:, None]).to(self.device)
-        # In this case, we want to track the gradients with respect to z,
-        # so specify that here
-        z_t = torch.autograd.Variable(self.z, requires_grad=True)
-        self.z_t = z_t
+        # (hence the [:, None]), and track the gradients with respect to z
+        self.z = torch.tensor(edges[:, None],
+                              requires_grad=True).to(self.device)
 
         ########################################
         # Material properties
         ########################################
+
         self.sigma_t = torch.from_numpy(sigma_t).unsqueeze(1).repeat(1, N)
         self.sigma_t = self.sigma_t.to(self.device)
 
@@ -123,25 +114,13 @@ class ANNSlabSolver(object):
 
         self._build_model(summary_writer)
 
-        self.learning_rate = learning_rate
         self.optimizer = torch.optim.Adam(self.model.parameters(),
                                           lr=learning_rate)
 
-        # Set regularization coefficients
         self.gamma_l = gamma_l
         self.gamma_r = gamma_r
 
-        # Set the convergence criterion
         self.eps = eps
-
-        self.use_weights = use_weights
-
-        num_points_z = len(self.z)
-        self.gamma = \
-            torch.ones(num_points_z, device=self.device).reshape(-1, 1)
-
-        ones = torch.ones(num_points_z, device=self.device).reshape(-1, 1)
-        self.r_squared_opt = (eps / num_points_z * ones)
 
         self.interval = interval
 
@@ -150,14 +129,14 @@ class ANNSlabSolver(object):
         self.hinterval = hinterval
 
     def _build_model(self, summary_writer=None):
-        """
+        '''
         Build neural network model.
-        """
+        '''
         model = torch.nn.Sequential(torch.nn.Linear(1, self.n_nodes),
                                     torch.nn.Tanh(),
                                     torch.nn.Linear(self.n_nodes, self.N),)
         if summary_writer:
-            summary_writer.add_graph(model, self.z_t)
+            summary_writer.add_graph(model, self.z)
 
         if self.gpu:
             self.model = model.cuda()
@@ -165,46 +144,33 @@ class ANNSlabSolver(object):
             self.model = model
 
     def _loss(self, y_pred, z):
-        """
+        '''
         Loss function for the network
-        """
+        '''
 
         # Calculate the isotropic flux
         phi_0 = self._compute_scalar_flux(psi=y_pred).reshape(-1, 1)
-        phi_1 = torch.matmul(y_pred, self.mu_t * self.w_t).reshape(-1, 1)
+        phi_1 = torch.matmul(y_pred, self.mu * self.w).reshape(-1, 1)
 
         # Create a placeholder for the gradient
         grad = torch.empty_like(y_pred, device=self.device)
 
         # Compute the gradient of each output with respect to the input
-        for idx in range(y_pred.shape[1]):
+        for angle_index in range(y_pred.shape[1]):
             grad_outputs = y_pred.data.new(y_pred.shape[0]).fill_(1)
-            g, = torch.autograd.grad(y_pred[:, idx], z,
+            g, = torch.autograd.grad(y_pred[:, angle_index], z,
                                      grad_outputs=grad_outputs,
                                      create_graph=True)
-            grad[:, idx] = g.flatten()
+            grad[:, angle_index] = g.flatten()
 
         # Compute the loss
-        l1 = (self.mu_t * grad + self.sigma_t * y_pred
-              - 0.5 * (self.sigma_s0 * phi_0
-                       + 3 * self.mu_t * self.sigma_s1 * phi_1)
-              - 0.5 * self.Q_t)**2
+        angular_loss = (self.mu * grad + self.sigma_t * y_pred
+                        - 0.5 * (self.sigma_s0 * phi_0
+                                 + 3 * self.mu * self.sigma_s1 * phi_1)
+                        - 0.5 * self.Q_t)**2
 
-        spatial_loss = l1.sum(1)
-        self.r_squared = spatial_loss.reshape(-1, 1)
-        loss = 0.5 * torch.dot(self.gamma.flatten(),
-                               self.r_squared.flatten()).reshape(1)
-
-        # Use the previous squared error as the weights
-        if self.use_weights:
-            ones = torch.ones_like(self.r_squared, device=self.device)
-            log1 = torch.log(self.r_squared_opt, device=self.device)
-            log2 = torch.log(self.r_squared, device=self.device)
-            rho = torch.max(ones, torch.abs(log1 / log2))
-
-            ones = torch.ones_like(rho, device=self.device)
-            omega = phi_0 / phi_0.max()
-            self.gamma = torch.max(ones, rho / omega, device=self.device)
+        spatial_loss = angular_loss.sum(1)
+        loss = 0.5 * spatial_loss.sum(0).reshape(1)
 
         # Add a penalty relating to the boundary conditions
         loss += (0.5 * self.gamma_l *
@@ -220,9 +186,9 @@ class ANNSlabSolver(object):
         return torch.sum(loss), sloss
 
     def train(self, num_iterations_estimate=2**20):
-        """
+        '''
         Train the neural network.
-        """
+        '''
 
         loss_history = np.zeros(num_iterations_estimate)
 
@@ -231,14 +197,14 @@ class ANNSlabSolver(object):
             spatial_loss_history = np.zeros(shape)
             flux_history = np.zeros(shape)
 
-        prev_loss = 1e6
+        prev_loss = LARGE_DOUBLE
         it = 0
         while True:
             # First, compute the estimate, which is known as the forward pass
-            y_pred = self.model(self.z_t)
+            y_pred = self.model(self.z)
 
             # Compute the loss between the prediction and true value
-            loss, spatial_loss = self._loss(y_pred, self.z_t)
+            loss, spatial_loss = self._loss(y_pred, self.z)
             loss_history[it] = loss
 
             # Inspect the value of the loss
@@ -280,26 +246,28 @@ class ANNSlabSolver(object):
         return result
 
     def _compute_scalar_flux(self, z=None, psi=None, numpy=False):
-        """
+        '''
         Compute the scalar flux at points z.
 
         Parameters
         ==========
         z : torch tensor, shape (1, n_points_z)
             Spatial variable to compute flux at.
-        numpy : bool, default=False
+        psi : torch tensor, shape (n_points_z, num_ordinates)
+            Angular flux
+        numpy : bool
             If True, return a numpy array. Otherwise return a torch tensor.
 
         Returns
         =======
         phi_0 : array-like, shape (n_points_z)
             The predicted scalar flux from the neural network at each z-point.
-        """
+        '''
 
         if psi is None:
             psi = self.model(z)
 
-        phi_0 = torch.matmul(psi, self.w_t)
+        phi_0 = torch.matmul(psi, self.w)
 
         if numpy:
             if self.gpu:
@@ -310,7 +278,7 @@ class ANNSlabSolver(object):
             return phi_0
 
     def predict(self, z=None):
-        """
+        '''
         Predict the flux at spatial positions z. If z is None, use the
         same spatial variables that were used in training the network.
 
@@ -322,12 +290,12 @@ class ANNSlabSolver(object):
         Returns
         =======
         phi : array-like, shape (n_points_z)
-            The predicted flux from the neural network at each z-point.
-        """
+            The predicted scalar flux from the neural network at each z-point.
+        '''
 
         # Use the existing spatial values if none are provided
         if z is None:
-            return self._compute_scalar_flux(z=self.z_t, numpy=True)
+            return self._compute_scalar_flux(z=self.z, numpy=True)
 
         # Otherwise, compute flux on the new spatial values
         arg = torch.tensor(z[:, None], device=self.device)
